b171728a77da848fa9379a27b2d3c52455171df7
yulng <yulng2000@163.com>
2018-03-13 16:40:44 +0800

    0313

--- autoTestMaster/src/common/Base.java
+++ autoTestMaster/src/common/Base.java
@@ -13,8 +13,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import common.Para;
 
 public class Base {
+	
+	/**
+	 * 
+	 * @author yangw
+	 * @version 1.00
+	 * @project DCE
+	 */
+	
 	@Test
 	public WebDriver loginDce(WebDriver driver) throws Exception {
 		
@@ -38,12 +47,30 @@ public class Base {
 	
 	@Test
 	public WebDriver loginDceFromExel(WebDriver driver) throws Exception {
+		Para paraValue = new Para();
+		String[] value = paraValue.paraSeting();
 		
+		System.setProperty("webdriver.gecko.driver", "/Users/yangwei/Documents/seleniumfile/geckodriver");
+		driver =new FirefoxDriver();
 		
-		return driver;
+		//登录dce
+		driver.get(value[4]);
+		Thread.sleep(5000);
+		WebElement searchUsername = driver.findElement(By.xpath("//input[@messageplacement='top-end']"));//定位用户名
+		searchUsername.sendKeys(value[0]);
+		Thread.sleep(2000);
+		WebElement searchPassword = driver.findElement(By.xpath("//input[@type='password']"));//定位密码
+		searchPassword.sendKeys(value[1]);
+		Thread.sleep(2000);
+		WebElement submit = driver.findElement(By.xpath("//button[@class='dao-btn blue login-btn']"));//定位button
+		submit.click();
+		Thread.sleep(5000);
+		return driver;	
 	}
 	
+	
 	@Test
+	//判断创建元素是否存在
 	public static boolean isElementExsit(WebDriver driver, By locator) {
 		boolean flag = false;
 		try {
@@ -57,12 +84,13 @@ public class Base {
 	
 	
 	@Test
+	//写入文件
 	public void rwFile(String cases, String message, String value) {
 		FileWriter fw = null;
 		try {
-			fw = new FileWriter("/Users/yangwei/Documents/dce_autotest.log", true);
-			String valuestring = String.valueOf(value);
-			fw.write(cases + "" + message + "" + valuestring + "\r\n");
+			fw = new FileWriter("/Users/yangwei/Documents/seleniumfile/dce_autotest.log", true);
+			String valueString = String.valueOf(value);
+			fw.write(cases + "" + message + "" + valueString + "\r\n");
 			fw.flush();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -82,6 +110,7 @@ public class Base {
 	}
 	
 	@Test
+	//根据reValue值，将结果写入log文件
 	public void writeLog(String cases,boolean reValue)throws Exception{	
 		
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
@@ -92,7 +121,5 @@ public class Base {
 			rwFile(cases , df.format(new Date()) , "Fail");
 		}
 	}
-	
-	
-	
+		
 }
--- autoTestMaster/src/dce/app/CreateApp.java
+++ autoTestMaster/src/dce/app/CreateApp.java
@@ -14,82 +14,85 @@ import org.testng.annotations.BeforeTest;
 
 @Test 
 public class CreateApp {
-
+	
+	/**
+	 * 
+	 * @author yangw
+	 * @version 1.00
+	 * @project DCE
+	 */
+	
 	Base pubMeth=new Base();
 	WebDriver driver;
 	public void createApp() throws Exception {
 		
+		//driver = pubMeth.loginDce(driver);
+		driver = pubMeth.loginDceFromExel(driver);
 		
-		driver = pubMeth.loginDce(driver);
 		pubMeth.rwFile("+++++++++++++++++", "+++++++++++++++++", "+++++++++++++++++");
 		//创建应用
 		//进入应用菜单
-		WebElement appcenter = driver.findElement(By.xpath("/html/body/div[2]/div[1]/nav/div[4]/div/div/span[1]"));//定位应用菜单
+		WebElement appCenter = driver.findElement(By.xpath("/html/body/div[2]/div[1]/nav/div[4]/div/div/span[1]"));//定位应用菜单
 		
 		//WebElement appcenter = driver.findElement(By.xpath("//div[@id='app']/div/nav/div[4]/div/div/span"));
-		appcenter.click();
+		appCenter.click();
 		Thread.sleep(2000);
 		//点部署应用
-		WebElement createapp = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div[1]/button[1]"));
-		createapp.click();
+		WebElement createApp = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div[1]/button[1]"));
+		createApp.click();
 		Thread.sleep(2000);
 		//选择一键部署
-		WebElement onebutton = driver.findElement(By.xpath("//input[@value='1']"));
-		onebutton.click();
+		WebElement oneButton = driver.findElement(By.xpath("//input[@value='1']"));
+		oneButton.click();
 		Thread.sleep(2000);
 		//继续
-		WebElement goon = driver.findElement(By.xpath("//button[@class='dao-btn blue has-icon compact']"));
-		goon.click();
+		WebElement goOn = driver.findElement(By.xpath("//button[@class='dao-btn blue has-icon compact']"));
+		goOn.click();
 		Thread.sleep(2000);
 		//搜索输入应用名称
-		WebElement searchapp = driver.findElement(By.xpath("//input[@placeholder='搜索应用模版']"));
-		searchapp.sendKeys("2048");
+		WebElement searchApp = driver.findElement(By.xpath("//input[@placeholder='搜索应用模版']"));
+		searchApp.sendKeys("2048");
 		Thread.sleep(2000);
 		//选择2048	
 		WebElement select2048 = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div/div/div/div[2]/div[2]"));
 		select2048.click();
 		Thread.sleep(2000);
 		//继续
-		WebElement goon2 = driver.findElement(By.xpath("//button[@class='dao-btn blue has-icon compact']"));
-		goon2.click();
+		WebElement goOn2 = driver.findElement(By.xpath("//button[@class='dao-btn blue has-icon compact']"));
+		goOn2.click();
 		Thread.sleep(2000);
 		//输入应用名称
-		WebElement inputname = driver.findElement(By.xpath("//input[@message='应用名必须以字母开头']"));
-		inputname.clear();
-		inputname.sendKeys("autotest2048");
+		WebElement inputName = driver.findElement(By.xpath("//input[@message='应用名必须以字母开头']"));
+		inputName.clear();
+		inputName.sendKeys("autotest2048");
 		Thread.sleep(2000);
 		//继续
-		WebElement goon3 = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div[2]/div[3]/button[2]"));
-		goon3.click();
+		WebElement goOn3 = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div[2]/div[3]/button[2]"));
+		goOn3.click();
 		Thread.sleep(2000);
 		//检查并创建		
 		//WebElement checkcreate = driver.findElement(By.xpath("//button[@class='dao-btn blue']"));
-		WebElement checkcreate = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/div[2]/button[2]"));
+		WebElement checkCreate = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/div[2]/button[2]"));
 		
-		checkcreate.click();
+		checkCreate.click();
 		Thread.sleep(5000);
 		//进入应用菜单
-		WebElement checkapp = driver.findElement(By.xpath("/html/body/div[2]/div[1]/nav/div[4]/div/div/span[1]"));
-		checkapp.click();
+		WebElement checkApp = driver.findElement(By.xpath("/html/body/div[2]/div[1]/nav/div[4]/div/div/span[1]"));
+		checkApp.click();
 		Thread.sleep(5000);
 		
+		//判断应用是否存在
 		By locator = By.xpath("//a[@href='#/app-detail/autotest2048']");
 		boolean reValue = pubMeth.isElementExsit(driver, locator);
 		System.out.println(reValue);
-		
+		//将结果写入log文件
 		pubMeth.writeLog("CreateApp ", reValue);
-		
+		//刷新
 		WebElement refresh = driver.findElement(By.xpath("//button[@title='刷新']"));
 		refresh.click();
 		Thread.sleep(3000);
 		
     //    driver.quit();
 	}
-
-	//private static WebDriver ChromeDriver() {
-		// TODO Auto-generated method stub
-	//	return null;
-	//}
-	
 
 }
